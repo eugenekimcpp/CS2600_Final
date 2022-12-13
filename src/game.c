e7ea7a4a1581e3f92c2ad1e702ddadd354d07e4b
Eugene Kim <eugenekim@cpp.edu>
2022-12-12 16:41:00 -0800

    function prototyes added

--- src/game.c
+++ src/game.c
@@ -7,15 +7,38 @@
 */
 #include <stdio.h>
 
+// functoin prototypes
+int checkWinner();
+int isFull();
+void makeMove();
+void displayBoard();
+void resetBoard();
+
+
+// global variables
+const char O = 'O';
+const char X = 'X';
+char winner; // will be used to store user character
 
 int main()
 {
 
+    // variables
+    // use do while loop to mark the borad
 
+    int continue = 1; // check if esp send quit(0) otherwise conitnue
+    int player 
 
 
 
 
 
     return 0;
+}
+
+
+void board()
+{
+
+
 }
