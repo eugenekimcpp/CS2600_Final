64493998b6631017ea690f8a5216c39b39aafe27
Eugene Kim <eugenekim@cpp.edu>
2022-12-12 16:51:49 -0800

    general structure start

--- src/game.c
+++ src/game.c
@@ -6,6 +6,8 @@
 
 */
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 // functoin prototypes
 int checkWinner();
@@ -16,6 +18,7 @@ void resetBoard();
 
 
 // global variables
+char board[3][3];
 const char O = 'O';
 const char X = 'X';
 char winner; // will be used to store user character
@@ -26,8 +29,49 @@ int main()
     // variables
     // use do while loop to mark the borad
 
-    int continue = 1; // check if esp send quit(0) otherwise conitnue
-    int player 
+    int continue; // check if esp send quit(0) otherwise conitnue
+    int player; 
+
+    // seed RNG
+    srand(time(0)); 
+
+    // game lobby
+    printf("Welcome to Tic-Tac-Toe game. \n
+            Waiting for user1 to start the game...");
+    // *******ESP CODE HERE that modity the continue
+    if(continue == 0)
+    {
+        printf("Thanks for playing!\n");
+        return 0;
+    }
+    else
+    {
+        // Enter here when user1 wants to play
+        do{
+        
+
+
+
+
+        // prompt user1 if want to continue
+        // ******ESP CODE HERE
+        if(continue == 0)
+        {
+            printf("Thanks for playing!\n");
+            return 0;
+        }
+        else
+        {
+            // if ESP choose to play more
+            resetBoard();
+        }
+
+      } while(continue == 1);  
+
+    }
+    
+    
+
 
 
 
